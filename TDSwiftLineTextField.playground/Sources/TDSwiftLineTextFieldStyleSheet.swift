b46bd144cc4ae203713b09dd9a8f8ec668140efd
michaeldai1006 <dtc74110@gmail.com>
2020-04-08 17:39:13 -0400

    Rearrange project content

--- TDSwiftLineTextField.playground/Sources/TDSwiftLineTextFieldStyleSheet.swift
+++ TDSwiftLineTextField.playground/Sources/TDSwiftLineTextFieldStyleSheet.swift
@@ -2,4 +2,14 @@ import Foundation
 import UIKit
 
 struct TDSwiftLineTextFieldSyleSheet {
+    struct color {
+        static let tintColor = UIColor(red:0.15, green:0.75, blue:0.85, alpha:1.0)
+        static let standByColor = UIColor(red:0.92, green:0.92, blue:0.96, alpha:0.6)
+        static let alertedColor = UIColor.red
+        static let textColor = UIColor.darkText
+    }
+    
+    struct size {
+        static let lineThickness: CGFloat = 2.0
+    }
 }
